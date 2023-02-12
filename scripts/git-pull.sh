df27e7adae96d2c4fdc487eca5adca7c4324a864
Cory Dransfeldt <hi@coryd.dev>
2023-02-11 16:16:47 -0800

    drop script

--- scripts/git-pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-SUBMODULES=('public/static/images/media' 'src/scripts' 'src/styles')
-git pull
-# && cd $SUBMODULE_MEDIA && git pull;
-for i in "${SUBMODULES}"
-do
-    cd "$i" && git pull;
-done
