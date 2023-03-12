f0ff047c63be08787d9bb230bcc3ea55051a2b42
Cory Dransfeldt <hi@coryd.dev>
2023-03-12 11:22:50 -0700

    update sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,37 +1,8 @@
 const siteMetadata = {
-  title: "Cory Dransfeldt",
-  author: "Cory Dransfeldt",
-  description: {
-    default:
-      "I'm a software developer in Camarillo, California. I enjoy hanging out with my beautiful family and 4 rescue dogs, technology, automation, music, writing, reading and tv and movies.",
-    referrals:
-      "Referral links for services I use. I save some money and you do as well if you choose to use them.",
-    uses: "Software and services that I use for work and my own enjoyment.",
-  },
-  themeColor: "#bd93f9",
+  title: "Cory Dransfeldt • social utils",
   siteUrl: "https://utils.coryd.dev",
-  siteDomain: "utils.coryd.dev",
-  blogUrl: "https://blog.coryd.dev",
-  blogFeeds: {
-    rss: "https://blog.coryd.dev/rss.xml",
-    atom: "https://blog.coryd.dev/atom.xml",
-    json: "https://blog.coryd.dev//feed.json",
-  },
-  icons: {
-    apple: "/static/favicons/apple-touch-icon.png",
-    favSm: "/static/favicons/favicon-16x16.png",
-    favLg: "/static/favicons/favicon-32x32.png",
-    favIco: "/static/favicons/favicon.ico",
-  },
-  socialBanner: "/social-card.png",
-  rootSiteUrl: "https://coryd.dev",
-  genericMediaImage: "https://utils.coryd.dev/media/404.jpg",
-  fathomCode: "RHNGSUXO",
-  fathomUrl: "https://breezy-restored.coryd.dev/script.js",
+  cdnUrl: "https://cdn.coryd.dev",
   language: "en-us",
-  social: {
-    mastodon: "https://social.lol/@cory",
-  },
 };
 
 module.exports = siteMetadata;
