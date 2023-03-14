612b17c912bad36ff85ea6f435a4a81ee023f278
Cory Dransfeldt <hi@coryd.dev>
2023-03-14 08:34:15 -0700

    reduce dependencies + add oku syndication

--- lib/syndicate/config.ts
+++ lib/syndicate/config.ts
@@ -4,10 +4,12 @@ export const SERVICES = {
   "coryd.dev": "https://coryd.dev/feed.xml",
   glass: "https://glass.photo/coryd/rss",
   letterboxd: "https://letterboxd.com/cdme/rss/",
+  oku: "https://oku.club/rss/collection/POaRa",
 };
 
 export const TAGS = {
   "coryd.dev": "#Blog",
   glass: "#Photo #Glass",
   letterboxd: "#Movie #Letterboxd",
+  oku: "#Books #Reading #Oku",
 };
